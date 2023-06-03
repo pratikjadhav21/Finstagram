f772729587695a35e0055cf35b1b63b045543b7c
PratikJadhav <pratikjadhav0815@gmail.com>
2023-06-03 15:29:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Finstagram
+
+A completely Responsive Instagram App- Works on Android, iOS & Web!
+
+## Features
+- Responsive Instagram UI
+- Email & Password Authentication
+- Share Posts with Caption
+- Display Posts with Caption
+- Like & Comment on Posts
+- Search Users
+- Follow Users
+- Display User Posts, Followers & Following
+- EVERYTHING REAL TIME
+- Sign Out
+
+Home Screen           |     Explore & Search Screen     |                   Profile Screen                 
+:-------------------------:|:-------------------------------:|:--------------------------------------------------:
+![](./screenshots/home.jpeg)  | ![](./screenshots/explore.jpeg) | ![](./screenshots/profile.jpeg)
+
+Upload Post Screen   | Select Photo Screen | Post Screen
+:-------------------------:|:-------------------------:|:-------------------------:
+![](./screenshots/upload.jpeg)  |  ![](./screenshots/select_photo.jpeg)  |  ![](./screenshots/post.jpeg)
+
+## Tech Used
+**Server**: Firebase Auth, Firebase Storage, Firebase Firestore
+
+**Client**: Flutter, Provider
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:finstagram/providers/user_provider.dart';
+import 'package:finstagram/responsive/mobile_screen_layout.dart';
+import 'package:finstagram/responsive/responsive_layout.dart';
+import 'package:finstagram/responsive/web_screen_layout.dart';
+import 'package:finstagram/screens/login_screen.dart';
+import 'package:finstagram/utils/colors.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDsCNOiCRSLgkbjkRQeoKZdg1i53i-RCmI",
+          appId: "1:39268192993:web:b9468a90d1ae86aa9d1da0",
+          messagingSenderId: "39268192993",
+          projectId: "finstagram-5f007",
+          storageBucket: "finstagram-5f007.appspot.com"
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider(),),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Finstagram',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              // Checking if the snapshot has any data or not
+              if (snapshot.hasData) {
+                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                return const ResponsiveLayout(
+                  mobileScreenLayout: MobileScreenLayout(),
+                  webScreenLayout: WebScreenLayout(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+
+            // means connection to future hasnt been made yet
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return const LoginScreen();
+          },
+        ),
+      ),
+    );
+  }
+}
